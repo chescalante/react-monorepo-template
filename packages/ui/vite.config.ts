c87e3645722fc2783cafef01b69f67e36cdd9b5c
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-05-30 15:17:19 -0300

    Add path aliases in ui package

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
